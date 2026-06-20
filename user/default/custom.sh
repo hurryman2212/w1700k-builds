c76ba45277dd710c7ee10a98db0b284f9a6d8d2a
fanboy <fanboy@openwrt.org>
2026-06-20 09:37:56 -1000

    include channel analysis patch (@Gilly1970)

--- user/default/custom.sh
+++ user/default/custom.sh
@@ -2,4 +2,6 @@
 echo "=============================================="
 echo "Running custom commands"
 mv files/overview.js feeds/luci/applications/luci-app-attendedsysupgrade/htdocs/luci-static/resources/view/attendedsysupgrade/overview.js
+mkdir -p feeds/luci/modules/luci-mod-status/patches
+mv files/998-single-wiphy.patch feeds/luci/modules/luci-mod-status/patches/998-single-wiphy.patch
 echo "=============================================="
